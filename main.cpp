2499a184c5e0e2c5c1c09e12286360b9a46f4abb
The-Last-Cookie <>
2020-10-07 18:16:12 +0200

    implement first participant selection

--- main.cpp
+++ main.cpp
@@ -17,6 +17,14 @@ int main() {
 
     std::cout << "Door #" << firstDoor + 1 << " contains: " << mod.showDoor(firstDoor) << "\n";
 
+    // select for the first time
+    // it does not matter which of the two closed doors is selected here
+    if (firstDoor == 0) {
+        participant.changeFocus(1); // can be either 1 or 2
+    } else {
+        participant.changeFocus(0);
+    }
+
     // template
     // if(door.content = "goat") {
     //    std::cout << "Moderator won\n";
