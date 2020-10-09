d098ec573b37e37d255c409637bca7926905a64b
The-Last-Cookie <>
2020-10-09 15:04:46 +0200

    add loop

--- main.cpp
+++ main.cpp
@@ -7,40 +7,48 @@
 
 #define SWITCH false
 
+// number of games played
+#define GAMES 10000
+
 int main() {
 
     Moderator mod;
     Participant participant;
 
     mod.shuffleDoors();
-    int firstDoor = mod.showFirstDoor();
+    
+    for (int i = 0; i < GAMES; i++) {
 
-    // select for the first time
-    // it does not matter which of the two closed doors is selected here
-    if (firstDoor == 0) {
-        participant.changeFocus(Random::randInt(1, 2));
-    }
-    else if (firstDoor == 2) {
-        participant.changeFocus(Random::randInt(0, 1));
-    }
-    else {
-        participant.changeFocus(Random::choice(0, 2));
-    }
+        int firstDoor = mod.showFirstDoor();
 
-    // switch or no switch?
-    bool switchDoor = SWITCH;
-    if (switchDoor) {
-        participant.switchDoor(firstDoor);
-    }
+        // select for the first time
+        // it does not matter which of the two closed doors is selected here
+        if (firstDoor == 0) {
+            participant.changeFocus(Random::randInt(1, 2));
+        }
+        else if (firstDoor == 2) {
+            participant.changeFocus(Random::randInt(0, 1));
+        }
+        else {
+            participant.changeFocus(Random::choice(0, 2));
+        }
 
-    if (mod.showDoor(participant.getFocusedDoor()) == "goat") {
-        participant.setFails(participant.getFailCounter() + 1);
-    }
-    else {
-        participant.setWins(participant.getWinCounter() + 1);
-    }
+        // switch or no switch?
+        bool switchDoor = SWITCH;
+        if (switchDoor) {
+            participant.switchDoor(firstDoor);
+        }
 
-    mod.clearDoors();
+        if (mod.showDoor(participant.getFocusedDoor()) == "goat") {
+            participant.setFails(participant.getFailCounter() + 1);
+        }
+        else {
+            participant.setWins(participant.getWinCounter() + 1);
+        }
+
+        mod.clearDoors();
+
+    }
 
     std::cout << "Wins: " << participant.getWinCounter() << "\n";
     std::cout << "Fails: " << participant.getFailCounter() << "\n";
